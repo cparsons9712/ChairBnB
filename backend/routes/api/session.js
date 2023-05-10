c4e9623020ae1d120872a54092278e7ba4d105d4
christine parsons <clong9712@gmail.com>
2023-05-10 18:35:11 -0400

    Added endpoint to get current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -5,6 +5,7 @@ const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 const router = express.Router();
 
+//sign in
 router.post(
     '/',
     async (req, res, next) => {
@@ -39,15 +40,31 @@ router.post(
         user: safeUser
       });
     }
-  );
+);
 
-  router.delete(
+//signout
+router.delete(
     '/',
     (_req, res) => {
       res.clearCookie('token');
       return res.json({ message: 'success' });
     }
-  );
+);
+
+//get session user
+router.get('/', (req,res) => {
+  const { user } = req;
+  if (user) {
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    return res.json({
+      user: safeUser
+    });
+  } else return res.json ({ user: null})
+});
 
 
 
