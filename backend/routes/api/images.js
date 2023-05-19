d1dfeb1f3f9c91c169a407405f9957fb23e51f19
christine parsons <clong9712@gmail.com>
2023-05-18 21:22:46 -0400

    Add conditional logic to seperate review images from spot images

--- backend/routes/api/images.js
+++ backend/routes/api/images.js
@@ -1,53 +1,52 @@
 const express = require('express');
 const { Image, Spot } = require('../../db/models');
-const {requireAuth} = require('../../utils/auth')
+
 
 
 const router = express.Router();
 
-router.delete('/:id', async (req, res, next)=> {
 
-    // spot ownerId must match signedin user id
+/*******************************************
+                DELETE AN IMAGE
+ ******************************************/
+router.delete('images/:type/:id', async (req, res, next)=> {
+    // Throw an error if a user is not signed in
+    if(!req.user){
+        const autherr = new Error()
+        autherr.status = 403
+        autherr.message = "Authentication required"
+        return next(autherr)
+    }
+    // check if this is the url for spot images
+    if (req.params.type === "spot"){
 
-
-        if(!req.user){
-            const autherr = new Error()
-            autherr.status = 403
-            autherr.message = "Authentication required"
-            return next(autherr)
-        }
+        // Get the user ID
         const userId = req.user.id
+        // Get the image and related Spot info
         const image = await Image.findOne({where: {id: req.params.id}, include: Spot})
-
-
-
-
+        // check that the image was found
         if (image) {
-
-
+            // Throw an error if the current user does not own the spot
             if( userId !== image.Spot.ownerId){
                 const permerr = new Error();
-                permerr.status = 403;
-                permerr.message = "Forbidden"
+                permerr.status = 403;permerr.message = "Forbidden";
                 return next(permerr)
+            }// Otherwise delete the image and send a response
+            else {
+                await image.destroy();
+                return res.json({"message": "Successfully deleted"})
             }
+        } // throw an error if the image isn't found
         else {
-            await image.destroy();
-            return res.json({"message": "Successfully deleted"})
-        }
-
-        }else {
             const err = new Error();
             err.status = 404;
-            err.message =  "Spot Image couldn't be found";
+            err.message =  "Image couldn't be found";
             return next(err);
         }
+    }
+    if (req.params.type === "review"){
 
-
-
-
-
-
+    }
 })
 
 
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -5,4 +5,7 @@ const router = express.Router();
 
 
 
+
+
+
 module.exports = router
