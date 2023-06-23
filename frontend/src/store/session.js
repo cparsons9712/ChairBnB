a001ab5192e1ec29577a78a8f388099d8cc05dcc
christine parsons <clong9712@gmail.com>
2023-06-22 20:38:42 -0400

    signup page done

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -3,6 +3,7 @@ import { csrfFetch } from "./csrf";
 const SET_USER = "session/setUser";
 const REMOVE_USER = "session/removeUser";
 
+
 const setUser = (user) => {
   return {
     type: SET_USER,
@@ -15,6 +16,8 @@ const removeUser = () => {
     type: REMOVE_USER,
   };
 };
+
+
 
 export const login = (user) => async (dispatch) => {
   const { credential, password } = user;
@@ -32,6 +35,19 @@ export const login = (user) => async (dispatch) => {
 
 export const restoreUser = () => async (dispatch) => {
   const response = await csrfFetch("/api/session");
+  const data = await response.json();
+  dispatch(setUser(data.user));
+  return response;
+};
+
+export const signup = (user) => async (dispatch) => {
+  const { username, firstName, lastName, email, password } = user;
+  const response = await csrfFetch("/api/users", {
+    method: "POST",
+    body: JSON.stringify({
+      username, firstName, lastName, email, password
+    }),
+  });
   const data = await response.json();
   dispatch(setUser(data.user));
   return response;
